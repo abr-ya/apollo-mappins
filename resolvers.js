a560f1f1ef1fb9cdef9a2325c8a800d20eca6a26
abr-ya <gamer05@list.ru>
2020-08-04 23:29:31 +0300

    Add Del Pin Functional and Selected Pin in the Blog

--- resolvers.js
+++ resolvers.js
@@ -24,6 +24,10 @@ module.exports = {
             }).save();
             const pinAdded = await Pin.populate(newPin, 'author');
             return pinAdded;
-        })
+        }),
+        deletePin: autenticated(async (root, args, ctx) => {
+            const pinDeleted = await Pin.findOneAndDelete({_id: args.pinId}).exec();
+            return pinDeleted;
+        }),
     }
 };
